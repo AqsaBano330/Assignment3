24fd301ec9c97516b227bccacf93ce350e9f62f0
AqsaBano330 <aqsabano08@gmail.com>
2023-06-24 23:56:37 +0500

    This is Assignment 3

--- question6.dart
+++ question6.dart
@@ -1,34 +1,34 @@
-//Create Map variable name world then inside it create countries Map, Key will be the name
-//country & country value will have another map having capitalCity, currency and language
-// to it. by using any country key print all the value of Capital & Currency.
+// //Create Map variable name world then inside it create countries Map, Key will be the name
+// //country & country value will have another map having capitalCity, currency and language
+// // to it. by using any country key print all the value of Capital & Currency.
 
-void main() {
-  Map<String, Map<String, dynamic>> world = {
-    "countries": {
-      "USA": {
-        "capitalCity": "Washington D.C.",
-        "currency": "USD",
-        "language": "English",
-      },
-      "France": {
-        "capitalCity": "Paris",
-        "currency": "EUR",
-        "language": "French",
-      },
-      "Japan": {
-        "capitalCity": "Tokyo",
-        "currency": "JPY",
-        "language": "Japanese",
-      },
-    }
-  };
+// void main() {
+//   Map<String, Map<String, dynamic>> world = {
+//     "countries": {
+//       "USA": {
+//         "capitalCity": "Washington D.C.",
+//         "currency": "USD",
+//         "language": "English",
+//       },
+//       "France": {
+//         "capitalCity": "Paris",
+//         "currency": "EUR",
+//         "language": "French",
+//       },
+//       "Japan": {
+//         "capitalCity": "Tokyo",
+//         "currency": "JPY",
+//         "language": "Japanese",
+//       },
+//     }
+//   };
 
-  String countryKey = "USA"; // Replace with the desired country key
+//   String countryKey = "USA"; // Replace with the desired country key
 
-  String capitalCity = world["countries"][countryKey]["capitalCity"];
-  String currency = world["countries"][countryKey]["currency"];
+//   String capitalCity = world["countries"][countryKey]["capitalCity"];
+//   String currency = world["countries"][countryKey]["currency"];
 
-  print("Country: $countryKey");
-  print("Capital City: $capitalCity");
-  print("Currency: $currency");
-}
+//   print("Country: $countryKey");
+//   print("Capital City: $capitalCity");
+//   print("Currency: $currency");
+// }
